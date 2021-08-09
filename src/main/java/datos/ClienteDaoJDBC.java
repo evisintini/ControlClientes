05f238f0e0b602befed3f362109e93555df53793
Ezequiel Visintini <ezeq1802@gmail.com>
2021-08-09 11:02:16 -0300

    Clase DAO (Alta, baja  modificación)

---
+++ src/main/java/datos/ClienteDaoJDBC.java
@@ -0,0 +1,169 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos;
+
+import dominio.Cliente;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author 2w50 111606 Ezequiel Favio Visintini
+ */
+public class ClienteDaoJDBC {
+
+    private static final String SQL_SELECT = "select id_cliente, nombre, apellido, email, telefono, saldo "
+            + "from cliente";
+
+    private static final String SQL_SELECT_BY_ID = "select id_cliente, nombre, apellido, email, telefono, saldo "
+            + "from cliente where id_cliente = ?";
+
+    private static final String SQL_INSERT = "insert into cliente (nombre, apellido, email, telefono, saldo) "
+            + " values (?, ?, ?, ?, ?)";
+
+    private static final String SQL_UPDATE = "update cliente "
+            + " set nombre = ?, apellido = ?, email = ?, telefono = ?, saldo = ? where id_cliente = ?";
+
+    private static final String SQL_DELETE = "delete from cliente where id_cliente = ?";
+
+    public List<Cliente> listar() {
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Cliente cliente = null;
+        List<Cliente> clientes = new ArrayList<>();
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_SELECT);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                int idCliente = rs.getInt("id_cliente");
+                String nombre = rs.getString("nombre");
+                String apellido = rs.getString("apellido");
+                String email = rs.getString("email");
+                String telefono = rs.getString("telefono");
+                double saldo = rs.getDouble("saldo");
+
+                cliente = new Cliente(idCliente, nombre, apellido, email, telefono, saldo);
+                clientes.add(cliente);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            Conexion.close(rs);
+            Conexion.close(stmt);
+            Conexion.close(conn);
+        }
+        return clientes;
+    }
+
+    public Cliente encontrar(Cliente cliente) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_SELECT_BY_ID);
+            stmt.setInt(1, cliente.getIdCliente());
+            rs = stmt.executeQuery();
+            rs.absolute(1); //nos posicionamos en el primer registro. Lo mismo que hace el método Next()
+
+            String nombre = rs.getString("nombre");
+            String apellido = rs.getString("apellido");
+            String email = rs.getString("email");
+            String telefono = rs.getString("telefono");
+            double saldo = rs.getDouble("saldo");
+
+            cliente.setNombre(nombre);
+            cliente.setApellido(apellido);
+            cliente.setEmail(email);
+            cliente.setTelefono(telefono);
+            cliente.setSaldo(saldo);
+
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            Conexion.close(rs);
+            Conexion.close(stmt);
+            Conexion.close(conn);
+        }
+        return cliente;
+    }
+
+    public int insertar(Cliente cliente) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int rows = 0; //Variable para saber cuantos registros se han modificado
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_INSERT);
+            stmt.setString(1, cliente.getNombre());
+            stmt.setString(2, cliente.getApellido());
+            stmt.setString(3, cliente.getEmail());
+            stmt.setString(4, cliente.getTelefono());
+            stmt.setDouble(5, cliente.getSaldo());
+
+            rows = stmt.executeUpdate();// El método executeUpdate() retorna la cantidad de registros que se han modificado
+
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            Conexion.close(stmt);
+            Conexion.close(conn);
+        }
+        return rows;
+    }
+
+    public int actualizar(Cliente cliente) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int rows = 0; //Variable para saber cuantos registros se han modificado
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_UPDATE);
+            stmt.setString(1, cliente.getNombre());
+            stmt.setString(2, cliente.getApellido());
+            stmt.setString(3, cliente.getEmail());
+            stmt.setString(4, cliente.getTelefono());
+            stmt.setDouble(5, cliente.getSaldo());
+            stmt.setInt(6, cliente.getIdCliente());
+
+            rows = stmt.executeUpdate();// El método executeUpdate() retorna la cantidad de registros que se han modificado
+
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            Conexion.close(stmt);
+            Conexion.close(conn);
+        }
+        return rows;
+    }
+
+    public int eliminar(Cliente cliente) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int rows = 0; //Variable para saber cuantos registros se han modificado
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_DELETE);
+            stmt.setInt(1, cliente.getIdCliente());
+
+            rows = stmt.executeUpdate();// El método executeUpdate() retorna la cantidad de registros que se han modificado
+
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            Conexion.close(stmt);
+            Conexion.close(conn);
+        }
+        return rows;
+    }
+
+}
