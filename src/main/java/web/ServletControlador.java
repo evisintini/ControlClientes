7a3b5b646245f0e6dac55037651dcbb7d7bfb27d
Ezequiel Visintini <ezeq1802@gmail.com>
2021-08-10 11:15:06 -0300

    editar cliente parte 1

--- src/main/java/web/ServletControlador.java
+++ src/main/java/web/ServletControlador.java
@@ -22,16 +22,27 @@ import javax.servlet.http.*;
 public class ServletControlador extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        this.accionDefault(request, response);
+        String accion = request.getParameter("accion");
+        if (accion != null) {
+            switch (accion) {
+                case "editar":
+                    this.editarCliente(request, response);
+                    break;
+                default:
+                    this.accionDefault(request, response);
+            }
+        } else {
+            this.accionDefault(request, response);
+        }
     }
 
-    private void accionDefault(HttpServletRequest request, HttpServletResponse response) 
+    private void accionDefault(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         List<Cliente> clientes = new ClienteDaoJDBC().listar();
         System.out.println("clientes = " + clientes);
-        
+
         HttpSession sesion = request.getSession();
         sesion.setAttribute("clientes", clientes); //entre paréntesis pongo el nombre de la lista, que es el que voy a poner como "items" en el jsp clientes.jsp. A la derecha el nombre de la lista que estoy insertando en ese nombre, que se lo doy en la linea -List<Cliente> --> clientes <-- = new ClienteDaoJDBC().listar();- 
         sesion.setAttribute("totalClientes", clientes.size());
@@ -46,6 +57,16 @@ public class ServletControlador extends HttpServlet {
             saldoTotal += cliente.getSaldo();
         }
         return saldoTotal;
+    }
+
+    private void editarCliente(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //Recuperamos el idCliente
+        int idCliente = Integer.parseInt(request.getParameter("idCliente"));
+        Cliente cliente = new ClienteDaoJDBC().encontrar(new Cliente(idCliente));
+        request.setAttribute("cliente", cliente);
+        String jspEditar = "/WEB_INF/paginas/cliente/editarCliente.jsp";
+        request.getRequestDispatcher(jspEditar).forward(request, response);
     }
 
     @Override
@@ -77,14 +98,14 @@ public class ServletControlador extends HttpServlet {
         if (saldoString != null && !"".equals(saldoString)) {
             saldo = Double.parseDouble(saldoString);
         }
-        
+
         //Creamos el objeto cliente (modelo)
         Cliente cliente = new Cliente(nombre, apellido, email, telefono, saldo);
-        
+
         //Insertamos el nevo objeto en la base de datos
         int registrosModificados = new ClienteDaoJDBC().insertar(cliente);
         System.out.println("registrosModificados = " + registrosModificados);
-        
+
         //Redirigimos a la accion por default
         this.accionDefault(request, response);
     }
